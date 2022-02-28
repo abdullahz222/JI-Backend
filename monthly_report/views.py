fd6f9cd7d47418309e332ef9eeb477a7621d3ce6
abdullah <abdullahzs222dot@gmail.com>
2022-02-28 08:29:20 -0800

    Created serializer views

--- monthly_report/views.py
+++ monthly_report/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from .models import Address, MemberData
+from .serializers import AddressSerializer, MemberDataSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class AddressList(generics.ListCreateAPIView):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+
+
+class AddressDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+
+
+class MemberDataList(generics.ListCreateAPIView):
+    queryset = MemberData.objects.all()
+    serializer_class = MemberDataSerializer
+
+
+class MemberDataDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MemberData.objects.all()
+    serializer_class = MemberDataSerializer
